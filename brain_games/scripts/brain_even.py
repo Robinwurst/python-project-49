dc39ca4fca531237a77acff4a162c3ad67fb3454
robinwurst <neorganizm911@yandex.ru>
2024-04-09 22:47:30 +0300

    Fix lint

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,18 +4,18 @@ import random
 
 
 def welcome():
-    print(f'Welcome to the Brain Games!\nMay I have your name?')
+    print("Welcome to the Brain Games!\nMay I have your name?")
     user_name = prompt.string(prompt=None, empty=False)
     print(f'Hello, {user_name}!')
 
 
 def even_odd():
     hidden_number = random.randint(0, 1000)
-    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     print(f'Question: {hidden_number}')
     answer = prompt.string(prompt=None, empty=False)
     print(f'Your answer: {answer}')
     if hidden_number % 2 == 0 and answer == 'yes':
         print('Correct!')
     else:
-        print(f"'yes' is wrong answer ;(. Correct answer was 'no'.")
+        print("'yes' is wrong answer ;(. Correct answer was 'no'.")
