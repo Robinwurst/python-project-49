1f6bc0021dac69f60a80e127eb95c34a29bb4c0d
Robinwurst <neorganizm911@yandex.ru>
2024-04-14 08:03:04 +0300

    Fix poetry error (toml)

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -10,6 +10,7 @@ def welcome():
 
 
 def even_odd():
+    prompt.PROMPT = 'Your answer? '
     hidden_number = random.randint(0, 1000)
     print('Answer "yes" if the number is even, otherwise answer "no".')
     print(f'Question: {hidden_number}')
@@ -17,5 +18,27 @@ def even_odd():
     print(f'Your answer: {answer}')
     if hidden_number % 2 == 0 and answer == 'yes':
         print('Correct!')
+        return True
+    elif hidden_number % 2 != 0 and answer == 'no':
+        print('Correct!')
+        return True
     else:
         print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+        return False
+
+
+def games_iter(true_answer=0):
+    result = even_odd()
+    while true_answer < 2:
+        if not result:
+            print("Try again!")
+            games_iter(0)
+        elif result:
+            true_answer += 1
+            print(f'{"Your score = " + str(true_answer)}')
+            games_iter(true_answer)
+        print('You win!')
+        return
+
+
+games_iter()
