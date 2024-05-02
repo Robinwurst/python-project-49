893c5b840c378d3f60fb823f2a905cfac5f8866e
robert <Neorganizm911@yandex.ru>
2024-05-02 22:20:46 +0300

    Test-check Fix

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -9,17 +9,17 @@ def start_game_calc(question):
     first_number = random.randint(1, 100)
     second_number = random.randint(1, 100)
     if rand_operation == 1:
-        print(f"{question} {first_number} + {second_number} = ")
+        print(f"{question} {first_number} + {second_number}")
         user_answer = prompt.integer(prompt=None, empty=False)
         true_answer = first_number + second_number
         return user_answer, true_answer
     elif rand_operation == 2:
-        print(f'{question}  {first_number} - {second_number} = ')
+        print(f'{question}  {first_number} - {second_number}')
         user_answer = prompt.integer(prompt=None, empty=False)
         true_answer = first_number - second_number
         return user_answer, true_answer
     elif rand_operation == 3:
-        print(f'{question}  {first_number} * {second_number} = ')
+        print(f'{question}  {first_number} * {second_number}')
         user_answer = prompt.integer(prompt=None, empty=False)
         true_answer = first_number * second_number
         return user_answer, true_answer
