ac11d6b9c8655f90066e996212cd3b7e53132280
robert <Neorganizm911@yandex.ru>
2024-05-02 22:08:06 +0300

    Test-check Fix

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,13 +4,13 @@ from brain_games.modules.game_modules import answer_check, welcome
 def engine(game_func, question):
     """Движок для игр. Принимает аргументы:
     true_answer, user_answer, question"""
-    welcome()
+    user_name = welcome()
     score = 0
     while score < 3:
         user_answer, true_answer = game_func(question)
         if answer_check(user_answer, true_answer):
             score += 1
-            print(f'Your score {score} from 3')
+            # print(f'Your score {score} from 3')
         else:
             return
-    print("You win!")
+    print(f'Congratulations, {user_name}!')
