812c2c9dd1ed243bf5ab57357cc4d2aeccd48816
robert <Neorganizm911@yandex.ru>
2024-05-02 22:14:16 +0300

    Test-check Fix

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,7 +8,7 @@ def engine(game_func, question):
     score = 0
     while score < 3:
         user_answer, true_answer = game_func(question)
-        if answer_check(user_answer, true_answer):
+        if answer_check(user_answer, true_answer, user_name):
             score += 1
             # print(f'Your score {score} from 3')
         else:
--- brain_games/modules/game_modules.py
+++ brain_games/modules/game_modules.py
@@ -1,29 +1,28 @@
 import prompt
 
-user_name = ""
 
 
 def welcome():
     prompt.PROMPT = ''
     print("Welcome to the Brain Games!\nMay I have your name?")
-    global user_name
     user_name = prompt.string(prompt=None, empty=False)
     print(f'Hello, {user_name}!')
+    return user_name
 
 
-def score_check(result_of_check_answer, true_answer=0):
-    while true_answer < 3:
-        result = result_of_check_answer
-        if result:
-            true_answer += 1
-            print(f'Your score = {true_answer}')
-        else:
-            print("Try again!")
-            return
-    print(f'Congratulations, {user_name}!')
+# def score_check(result_of_check_answer, true_answer=0):
+#     while true_answer < 3:
+#         result = result_of_check_answer
+#         if result:
+#             true_answer += 1
+#             print(f'Your score = {true_answer}')
+#         else:
+#             print("Try again!")
+#             return
+#     print(f'Congratulations, {user_name}!')
 
 
-def answer_check(user_answer, true_answer):
+def answer_check(user_answer, true_answer, user_name):
     if user_answer != true_answer:
         print(f'Your answer: {user_answer}')
         print(f"{user_answer}' is wrong answer ;(. "
