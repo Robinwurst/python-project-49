9c37c28640a248393b5e0c6b49ffc36e47961d57
robert <Neorganizm911@yandex.ru>
2024-05-04 17:03:34 +0300

    Test-check Fix

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,4 +1,25 @@
-from brain_games.modules.game_modules import answer_check, welcome
+import prompt
+
+
+def welcome():
+    prompt.PROMPT = ''
+    print("Welcome to the Brain Games!\nMay I have your name?")
+    user_name = prompt.string(prompt=None, empty=False)
+    print(f'Hello, {user_name}!')
+    return user_name
+
+
+def answer_check(user_answer, true_answer, user_name):
+    if user_answer != true_answer:
+        print(f'Your answer: {user_answer}')
+        print(f"'{user_answer}' is wrong answer ;(. "
+              f"Correct answer was '{true_answer}'.")
+        print(f"Let's try again, {user_name}!")
+        return False
+    elif user_answer == true_answer:
+        print(f'Your answer: {user_answer}')
+        print("Correct!")
+        return True
 
 
 def engine(game_func, question):
