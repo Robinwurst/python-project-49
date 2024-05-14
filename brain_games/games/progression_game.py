416595ad6e95094a4a6c8b172696ae6df625d083
robert <Neorganizm911@yandex.ru>
2024-05-14 06:36:09 +0300

    Fix project(logic of games)

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -2,6 +2,10 @@
 import random
 
 QUESTION = "What number is missing in the progression?"
+MIN_NUMBER_OF_PROGRESSION = 0
+MAX_NUMBER_OF_PROGRESSION = 150
+MIN_STEP_SIZE_OF_PROGRESSION = 1
+MAX_STEP_SIZE_OF_PROGRESSION = 10
 MIN_LEN_OF_PROGRESSION = 6
 MAX_LEN_OF_PROGRESSION = 11
 
@@ -14,7 +18,12 @@ def hide_number(cut_list, hidden_number):
 
 
 def gen_raund_data():
-    base_list_of_numbers = list(range(0, 150, random.randint(1, 10)))
+    base_list_of_numbers = list(range(
+        MIN_NUMBER_OF_PROGRESSION,
+        MAX_NUMBER_OF_PROGRESSION,
+        random.randint(MIN_STEP_SIZE_OF_PROGRESSION,
+                       MAX_STEP_SIZE_OF_PROGRESSION))
+    )
     random_len = random.randint(MIN_LEN_OF_PROGRESSION, MAX_LEN_OF_PROGRESSION)
     cut_list = base_list_of_numbers[:random_len]
     hidden_number = random.randint(0, random_len - 1)
